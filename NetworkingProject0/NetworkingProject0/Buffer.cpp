49f1371fc3154c05c0bf001d979293b884b084a8
WesBach <wes_bach@live.com>
2017-10-08 12:51:16 -0400

    buffer integer implementation

--- NetworkingProject0/NetworkingProject0/Buffer.cpp
+++ NetworkingProject0/NetworkingProject0/Buffer.cpp
@@ -6,16 +6,44 @@ Buffer::Buffer(size_t size) {
 }
 
 //int
-void Buffer::WriteInt32BE(size_t index, int value) {
+//INT32
+int Buffer::ReadInt32BE(void) {
+	int value = mBuffer[mReadIndex] << 24;
+	value |= mBuffer[mReadIndex + 1] << 16;
+	value |= mBuffer[mReadIndex + 2] << 8;
+	value |= mBuffer[mReadIndex + 3];
+	mReadIndex += 4;
+	return value;
+}
 
+int Buffer::ReadInt32BE(size_t index) {
+	//little endian is [index], [index] <<4, [index]<<16, [index]<<24
+	int value = mBuffer[index] << 24;
+	value |= mBuffer[index + 1] << 16;
+	value |= mBuffer[index + 2] << 8;
+	value |= mBuffer[index + 3];
 }
+
 void Buffer::WriteInt32BE(int value) {
+	mBuffer[mWriteIndex] = value >> 24;
+	++mWriteIndex;
+	mBuffer[mWriteIndex] = value >> 16; //bit shift by 8 to store 8 bits in each buffer slot
+	++mWriteIndex;
+	mBuffer[mWriteIndex] = value >> 8;
+	++mWriteIndex;
+	mBuffer[mWriteIndex] = value;
+	++mWriteIndex;
 }
-int  Buffer::ReadInt32BE(size_t index) {
-	return 1;
-}
-int Buffer::ReadInt32BE(void){
-	return 1;
+
+void Buffer::WriteInt32BE(size_t index, int value) {
+	mBuffer[index] = value >> 24;
+	++mWriteIndex;
+	mBuffer[index + 1] = value >> 16;
+	++mWriteIndex;
+	mBuffer[index + 2] = value >> 8;
+	++mWriteIndex;
+	mBuffer[index + 3] = value;
+	++mWriteIndex;
 }
 
 //unsigned short
@@ -25,7 +53,7 @@ void Buffer::WriteUShortBE(size_t index, unsigned short value) {
 void Buffer::WriteUShortBE(unsigned short value) {
 
 }
-unsigned short Buffer::(size_t index) {
+unsigned short Buffer::ReadUShortBE(size_t index) {
 	return 1;
 }
 unsigned short Buffer::ReadUShortBE(void) {
