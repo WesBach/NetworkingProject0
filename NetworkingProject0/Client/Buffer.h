49d7b8440317804b81d1a4a0205b906f2f956596
WesBach <wes_bach@live.com>
2017-10-09 20:22:58 -0400

    added client priject and its buffer header

---
+++ NetworkingProject0/Client/Buffer.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef Buffer_HG
+#define Buffer_HG
+#include<vector>
+class Buffer {
+public:
+	Buffer(size_t size);
+
+	//int
+	void WriteInt32BE(size_t index, int value);
+	void WriteInt32BE(int value);
+	int ReadInt32BE(size_t index);
+	int ReadInt32BE(void);
+
+	//unsigned short
+	void WriteUShortBE(size_t index, unsigned short value);
+	void WriteUShortBE(unsigned short value);
+	unsigned short ReadUShortBE(size_t index);
+	unsigned short ReadUShortBE(void);
+
+	//short
+	void WriteShortBE(size_t index, short value);
+	void WriteShortBE(short value);
+	short ReadShortBE(size_t index);
+	short ReadShortBE(void);
+
+	//TO DO: string conversion(not really converting anything)
+	void WriteStringBE(size_t index, std::string value);
+	void WriteStringBE(std::string value);
+	short ReadStringBE(size_t index);
+	short ReadStringBE(void);
+
+private:
+	std::vector<uint8_t> mBuffer;
+	int mReadIndex;
+	int mWriteIndex;
+};
+#endif // !Buffer_HG
