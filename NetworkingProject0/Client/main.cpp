afa3cd07b849e353f937aa968046f431153c27b8
BenCorso <Benjimancorso@gmail.com>
2017-10-12 17:16:36 -0400

    Added client code to let the user quit the program.

--- NetworkingProject0/Client/main.cpp
+++ NetworkingProject0/Client/main.cpp
@@ -1,8 +1,24 @@
 #include "Buffer.h"
 #include <string>
 #include <iostream>
+#include <conio.h>
 
 //TO DO: Client side connection
 int main() {
 
+
+	//Taking in user's input and quiting when detecting the 'q' (infinite loop)
+	while (true)
+	{
+		if (_kbhit()) 
+		{
+			char ch = _getch();
+			std::cout << ch;
+			if (ch == 'q')
+			{
+				std::cout << "QUIT" << std::endl;
+				break;
+			}
+		}
+	}
 }
